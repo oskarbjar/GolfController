0a5351e0c309c70f432846e13868d6c9c1fe84af
ornbja <ornbja@gmail.com>
2012-04-22 00:05:18 +0000

    setti return RedirectToAction("ViewHoles", new { id = hole.CourseID }) á allar Crud skipanir í Holecontroller

--- GolfController/Controllers/HoleController.cs
+++ GolfController/Controllers/HoleController.cs
@@ -25,7 +25,7 @@ namespace GolfController.Controllers
 
         public ViewResult ViewHoles(int id)
         {
-            var hole = db.hole.Where(m => m.CourseID == id);
+            var hole = db.hole.OrderBy(m=> m.HoleNumber).Where(m => m.CourseID == id);
                 return View ( hole.ToList());
         }
 
@@ -96,7 +96,7 @@ namespace GolfController.Controllers
                 db.Entry(hole).State = EntityState.Modified;
                 db.SaveChanges();
                 CalculatePar(hole);
-                return RedirectToAction("Index");
+                return RedirectToAction("ViewHoles", new { id = hole.CourseID });
             }
             ViewBag.CourseID = new SelectList(db.course, "ID", "Name", hole.CourseID);
             return View(hole);
@@ -121,7 +121,7 @@ namespace GolfController.Controllers
             db.hole.Remove(hole);
             db.SaveChanges();
             CalculatePar(hole);
-            return RedirectToAction("Index");
+            return RedirectToAction("ViewHoles", new { id = hole.CourseID });
         }
 
         protected override void Dispose(bool disposing)
